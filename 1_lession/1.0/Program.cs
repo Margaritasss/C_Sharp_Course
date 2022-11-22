ab270f787bd8664a617b9dc02bb9813804191f06
Margarita <margarita-24.04@mail.ru>
2022-11-22 20:56:44 +0300

    Task 1.0

--- 1_lession/1.0/Program.cs
+++ 1_lession/1.0/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число, которое хотите возвести в квадрат");
+double numberA = Convert.ToDouble(Console.ReadLine());
+double X = 1;
+string str1 = "";
+
+X = numberA * numberA;
+
+str1 = numberA + " -> " + X;
+
+Console.WriteLine(str1);
