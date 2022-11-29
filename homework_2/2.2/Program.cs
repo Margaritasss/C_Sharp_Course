75ba74edd249bfee06a3dc479db023ee9c848090
Margarita <margarita-24.04@mail.ru>
2022-11-29 21:08:15 +0300

    Task 2.2

---
+++ homework_2/2.2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 2:
+// Напишите программу, которая выводит случайное
+// трёхзначное число и удаляет вторую цифру этого числа.
+
+int answer (int RND)
+{
+    Console.WriteLine(RND);
+    return ((RND / 100) * 10 + (RND % 100) % 10);
+}
+
+Console.WriteLine("Введите число");
+int X = new Random().Next(100, 1000); //int.Parse(Console.ReadLine());
+Console.WriteLine("Число без второй цифры " + answer(X));
