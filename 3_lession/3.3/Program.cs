1f61d260206b49325df4f6394deb8b1df9552bf3
Margarita <margarita-24.04@mail.ru>
2022-12-07 01:40:05 +0300

    Task_3.3

---
+++ 3_lession/3.3/Program.cs
@@ -0,0 +1,14 @@
+﻿void Chet(int N)
+{
+    int i = 0;
+    double pere;
+    while (i < N)
+    {
+        i += 1;
+        pere = Math.Pow(i,2);
+        Console.Write($"{pere},");
+    }
+}
+Console.WriteLine("введите ч");
+int chislo = int.Parse(Console.ReadLine());
+Chet(chislo);
