27c9fd9c4807c566cb9ed348638cd077269b8215
Margarita <margarita-24.04@mail.ru>
2022-12-07 22:38:05 +0300

    Homework_task_4.2

---
+++ homework_4/4.2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+string answer(int num)
+{
+    int sum = 0;
+    int x = num;
+    int new_num = 0;
+
+    while (num > 0)
+    {
+        new_num = num % 10;
+        sum = sum + new_num;
+        num = num / 10;
+    }
+    return (x + " -> " + sum);
+}
+
+Console.WriteLine("Введите число: ");
+int A = int.Parse(Console.ReadLine());
+
+Console.WriteLine(answer(A));
