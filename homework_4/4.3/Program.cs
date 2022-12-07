189742b8c2ffff675ad5b700df61fd640bf28d86
Margarita <margarita-24.04@mail.ru>
2022-12-07 22:38:17 +0300

    Homework_task_4.3

---
+++ homework_4/4.3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран
+
+void answer(int A)
+{
+    string result = "";
+    int[] massiv = new int[A];
+    for (int i = 0; i < massiv.Length; i++)
+    {
+        massiv[i] = new Random().Next(1, 1000);
+        if (i == 0) result = "" + massiv[i];   
+        else result = result + " " + massiv[i];
+    }
+    Console.Write("[" + result + "]");
+}
+
+Console.Write("Введите число элементов массива: ");
+int x = int.Parse(Console.ReadLine());
+answer(x);
