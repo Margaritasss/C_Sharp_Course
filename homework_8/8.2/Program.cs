a3984de5a011be901c0c8dd05f585b1c0b89c255
Margarita <margarita-24.04@mail.ru>
2022-12-20 21:38:09 +0300

    Homework_task_8.2

---
+++ homework_8/8.2/Program.cs
@@ -0,0 +1,63 @@
+﻿// Задайте прямоугольный двумерный массив. Напишите программу, 
+// которая будет находить строку с наименьшей суммой элементов.
+
+void Print(int[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < column_size; j++)
+            Console.Write($" {arr[i, j],4} ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] MassNums(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            arr[i, j] = new Random().Next(from, to);
+
+    return arr;
+}
+
+void MinimalSum(int[,] arr)
+{
+    int minRows = 0;
+    int MinimalSum = 0;
+    int sumRows = 0;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        minRows += arr[0, i];
+    }
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            sumRows += arr[i, j];
+        }
+            if (sumRows < minRows)
+            {
+                minRows = sumRows;
+                MinimalSum = i;
+            }
+            sumRows = 0;
+    }
+    Console.Write($"{MinimalSum + 1} строка");
+}
+
+Console.Write("Enter the number of rows: ");
+int row = int.Parse(Console.ReadLine());
+Console.Write("Enter the number of columns: ");
+int column = int.Parse(Console.ReadLine());
+
+int[,] arr_1 = MassNums(row, column,
+                        int.Parse(Console.ReadLine()),
+                        int.Parse(Console.ReadLine()));
+Print(arr_1);
+MinimalSum(arr_1);
